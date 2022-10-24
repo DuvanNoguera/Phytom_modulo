745b28a3e9f99430ea286cd94e6b822e41adf7b4
DuvanNoguera <101603410+DuvanNoguera@users.noreply.github.com>
2022-10-24 12:58:21 -0500

    Add files via upload

---
+++ suma_resta.py
@@ -0,0 +1,11 @@
+import modulo_santiago
+
+#llamamos módulo de Santiago
+a = validacion()
+def suma(a):
+    monto = 0
+    if a == True:
+        return monto += 100
+    else:
+        return monto -= 100
+    print('Su monto es', monto)
